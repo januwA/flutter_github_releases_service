e6f54beb51fd5c72ad3e3226108b3fc16af9f9a5
januwA <ajanuw1641845087@sina.com>
2019-08-07 13:13:20 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# flutter_github_releases_service
+
+在flutter中每次更新版本，把打包后的apk放在GitHub的releases上
+
+此包的目的是检测当前用户使用的app版本，是否是在releases上的最新版本。
+
+并提供下载，更新，自动安装最新版的apk
---
+++ lib/flutter_github_releases_service.dart
@@ -0,0 +1,7 @@
+library flutter_github_releases_service;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
